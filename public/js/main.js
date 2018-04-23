3ada5931e3b5b3628b60f60651b8e602d286f013
Anna Boeri <annaboeri@gmail.com>
2018-04-23 13:16:43 -0700

    change grid col width

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,7 @@ var httpClient = axios.create()
 $feelsItem.on('click', function() {
     $resultsUl.empty()
     $resultsKey.empty()
+    $resultsInfo.empty()
     $resultsKey.removeClass("display-key")
     $resultsInfo.removeClass("display-info")
     const feel = $(this).text()
@@ -40,14 +41,15 @@ $resultsUl.on('click', 'li', function(){
     let $this = $(this)[0]
     const feel = $(this)[0].innerText
     const options = { url: `/details/${feel}` }
-    if (!$($this).attr("title")){
         httpClient(options).then((serverResponse) => {
             $resultsInfo.addClass("display-info")
-            $resultsInfo.text(serverResponse.data)
-             $($this).attr("title", serverResponse.data)
-             console.log($($this).attr("title") )
-           })
-    }
+            if (!serverResponse.data){
+                $resultsInfo.text("Sorry, no description is available for this strain.")
+            }
+            else {
+                $resultsInfo.text(serverResponse.data)
+            }
+        })
 })
 
 
